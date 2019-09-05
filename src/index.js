a81e43d0e9e918166355bc257b37195c9a19d181
Senku Ishigami <senku@localhost.localdomain>
2019-09-05 13:31:23 +0200

    ready to start

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// import styles
+import "./style.scss";
+
+// import javascript
+// import Unsplash from "unsplash-js";
+
+// // code
+// const unsplash = new Unsplash({
+//   applicationId:
+//     "7986c97eef88f3004ac3f75f9ce2209ac31e470b936cfcf84d757a1b28acf1f9",
+//   secret: "2086a5b65478d9465ee086e6f9333c4b079ce3413b7d09d009e6ec38d21fbf34"
+// });
+
+// const searchBox = document.getElementById("search");
+
+// searchBox.addEventListener("change", e => {
+//   console.log(e.target.value);
+// });
+// const img = document.getElementById("test");
+
+// unsplash.photos
+//   .getRandomPhoto()
+//   .then(Unsplash.toJson)
+//   .then(json => {
+//     console.log(json.url);
+//     img.src = json.url;
+//   });
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: {
+          loader: "css-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    require("autoprefixer")
+  ]
+};
