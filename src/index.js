a31f960dacd6bfce0b06a7a99f09089d27db39f5
Senku Ishigami <senku@localhost.localdomain>
2019-09-06 11:26:06 +0200

    infinite scroll

--- src/index.js
+++ src/index.js
@@ -15,16 +15,19 @@ const gallery = document.getElementById("gallery");
 
 const pexels_header = new Headers();
 
+let currentPage = 0;
+
 pexels_header.append("Authorization", cred.PEXELS_API_KEY);
 
 searchBox.addEventListener("change", e => {
   gallery.innerHTML = "";
+  currentPage = 1;
   fetch(
     "https://pixabay.com/api/?key=" +
       cred.PIXABAY_APP_ID +
       "&q=" +
       e.target.value +
-      "&page=1&per_page=3"
+      "&page=1&per_page=9"
   )
     .then(res => res.json())
     .then(data => {
@@ -37,7 +40,7 @@ searchBox.addEventListener("change", e => {
     });
 
   fetch(
-    "https://api.unsplash.com/photos/search?page=1&per_page=3&query=" +
+    "https://api.unsplash.com/photos/search?page=1&per_page=9&query=" +
       e.target.value +
       "&client_id=" +
       cred.UNSPLASH_APP_ID
@@ -55,7 +58,7 @@ searchBox.addEventListener("change", e => {
   fetch(
     "https://api.pexels.com/v1/search?query=" +
       e.target.value +
-      "&per_page=3&page=1",
+      "&per_page=9&page=1",
     {
       method: "GET",
       headers: pexels_header
@@ -71,3 +74,69 @@ searchBox.addEventListener("change", e => {
       });
     });
 });
+
+window.onscroll = e => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+    searchBox.value
+  ) {
+    currentPage++;
+    fetch(
+      "https://pixabay.com/api/?key=" +
+        cred.PIXABAY_APP_ID +
+        "&q=" +
+        searchBox.value +
+        "&page=" +
+        currentPage +
+        "&per_page=3"
+    )
+      .then(res => res.json())
+      .then(data => {
+        data.hits.map(pic => {
+          let newImg = document.createElement("img");
+          newImg.className = "pic";
+          newImg.src = pic.webformatURL;
+          gallery.appendChild(newImg);
+        });
+      });
+
+    fetch(
+      "https://api.unsplash.com/photos/search?page=" +
+        currentPage +
+        "&per_page=3&query=" +
+        searchBox.value +
+        "&client_id=" +
+        cred.UNSPLASH_APP_ID
+    )
+      .then(res => res.json())
+      .then(data => {
+        data.map(pic => {
+          let newImg = document.createElement("img");
+          newImg.className = "pic";
+          newImg.src = pic.urls.small;
+          gallery.appendChild(newImg);
+        });
+      });
+
+    fetch(
+      "https://api.pexels.com/v1/search?query=" +
+        searchBox.value +
+        "&per_page=3&page=" +
+        currentPage +
+        "",
+      {
+        method: "GET",
+        headers: pexels_header
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        data.photos.map(pic => {
+          let newImg = document.createElement("img");
+          newImg.className = "pic";
+          newImg.src = pic.src.medium;
+          gallery.appendChild(newImg);
+        });
+      });
+  }
+};
