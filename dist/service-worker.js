3d2ca3501cd78fd8d13b5badd2d37e2015891ea1
Senku <dorosario.enzo@protonmail.com>
2019-09-30 15:03:18 +0200

    modify service work

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,32 +1,19 @@
-const FILE_TO_CACHE = ["/offline.html", "/main.js"];
+const filesToCache = ["/offline.html", "/main.js"];
+const cacheName = "fap-cache";
 
-evt.waitUntil(
-  caches.open(CACHE_NAME).then(cache => {
-    console.log("[ServiceWorker] Pre-caching offline page");
-    return cache.addAll(FILES_TO_CACHE);
-  })
-);
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
-evt.waitUntil(
-  caches.keys().then(keyList => {
-    return Promise.all(
-      keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          console.log("[ServiceWorker] Removing old cache", key);
-          return caches.delete(key);
-        }
-      })
-    );
-  })
-);
-
-if (evt.request.mode !== "navigate") {
-  return;
-}
-evt.respondWith(
-  fetch(evt.request).catch(() => {
-    return caches.open(CACHE_NAME).then(cache => {
-      return cache.match("offline.html");
-    });
-  })
-);
+/* Serve cached content when offline */
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
