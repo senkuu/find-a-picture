1fa8f4577e139007cd1225c2831cc3223cd8b539
Senku <dorosario.enzo@protonmail.com>
2019-10-03 12:07:11 +0200

    clean tests for themeChanger

--- tests/themeChanger.test.js
+++ tests/themeChanger.test.js
@@ -1,31 +1,24 @@
+import "@testing-library/jest-dom/extend-expect";
 import themeChanger from "../src/js/themeChanger";
 
-describe("Change theme", () => {
-  it("Sould change dark theme to light theme", () => {
-    const icon = document.createElement("i");
+describe("Test testChanger function", function() {
+  it("Should change dark theme to light theme", function() {
     document.body.className = "dark";
-    themeChanger(icon);
-    expect(document.body.className).toBe("light");
-  });
-
-  it("Sould change light theme to dark theme", () => {
-    const icon = document.createElement("i");
-    document.body.className = "light";
-    themeChanger(icon);
-    expect(document.body.className).toBe("dark");
-  });
-
-  it("Should change sun icon to moon icon", () => {
     const icon = document.createElement("i");
     icon.className = "fa fa-sun-o";
     themeChanger(icon);
-    expect(icon.className).toBe("fa fa-moon-o");
+
+    expect(document.body).toHaveClass("light");
+    expect(icon).toHaveClass("fa fa-moon-o");
   });
 
-  it("Should change moon icon to sun icon", () => {
+  it("Should change light theme to dark theme", function() {
+    document.body.className = "light";
     const icon = document.createElement("i");
     icon.className = "fa fa-moon-o";
     themeChanger(icon);
-    expect(icon.className).toBe("fa fa-sun-o");
+
+    expect(document.body).toHaveClass("dark");
+    expect(icon).toHaveClass("fa fa-sun-o");
   });
 });
